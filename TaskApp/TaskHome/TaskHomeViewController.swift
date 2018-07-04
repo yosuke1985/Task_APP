1e953160d9ed4071723f133d9866d082a1391def
YOSUKE NAKAYAMA <ihatenonoildressing@gmail.com>
2018-07-04 21:32:17 +0900

    just upload to Firebase

--- TaskApp/TaskHome/TaskHomeViewController.swift
+++ TaskApp/TaskHome/TaskHomeViewController.swift
@@ -8,23 +8,68 @@
 
 import UIKit
 import RealmSwift
+import Firebase
+import SwiftyJSON
+
+
 
 class TaskHomeViewController: UIViewController,UITableViewDelegate, UITableViewDataSource, AddTaskTableViewCellDelegate,TaskListTableViewCellDelegate{
 
-    
-
+    let realm = try! Realm()
     @IBOutlet weak var tableView: UITableView!
     var allTask:[Task]?
     var taskCount:Int = 0
+    var ref:DatabaseReference!
+
+    let formatter = DateFormatter()
     
     override func viewDidLoad() {
         tableView.delegate = self
         tableView.dataSource = self
-
         tableView.register(UINib(nibName: "AddTaskTableViewCell", bundle: nil), forCellReuseIdentifier: "addTask")
         tableView.register(UINib(nibName: "TaskListTableViewCell", bundle: nil), forCellReuseIdentifier: "taskList")
         reload()
-    
+        //Firebaseにアップロードする
+        ref = Database.database().reference()
+        // Realm からデータの取得
+        let objects = Task.loadAll()
+        print("objects", objects)
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+ 
+        for object in objects{
+            print(object.task_name)
+            var data:[String:Any] = [:]
+            data["task_name"] = object.task_name
+            data["notes"] = object.notes
+            data["list"] = object.list
+            data["done"] = object.done
+            if object.UpdatedAt != nil{
+                let myString = formatter.string(from: object.UpdatedAt! as Date)
+                data["UpdatedAt"] = myString
+            }
+            
+            if object.CreatedAt != nil{
+                let myString = formatter.string(from: object.CreatedAt! as Date)
+                data["CreatedAt"] = myString
+            }
+            ref.child("Task").child(String(object.id)).updateChildValues(data)
+        }
+        
+        //取得
+        ref.child("Task").observe(.value , with: { (snapshot: DataSnapshot) in
+            //JSON形式でもらいたい　オートIDから
+            let getjson = JSON(snapshot.value as? [String : AnyObject] ?? [:])
+            //データが0件の場合何もしない
+            if getjson.count == 0 { return }
+            //keyから辞書型、"msg"の内容を代入 "\n"は改行
+            for (key, val) in getjson.dictionaryValue {
+                //key情報と入力した文字を表示する
+                print("key",key)
+                print("value",val)
+            }
+        })
+        
+        
         super.viewDidLoad()
     }
     
@@ -57,7 +102,7 @@ class TaskHomeViewController: UIViewController,UITableViewDelegate, UITableViewD
         print(nextVC.view)
         print("prepare(for segue", sender!)
 
-        nextVC.task = sender as! Task
+        nextVC.task = sender as? Task
         
     }
     
