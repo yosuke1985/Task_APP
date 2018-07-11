757dfc6588c2e6431aaf4dca0ef1b8f6701390f0
YOSUKE NAKAYAMA <ihatenonoildressing@gmail.com>
2018-07-11 09:02:02 +0900

    Task model, User model

---
+++ TaskApp/Model/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  TaskApp
+//
+//  Created by YOSUKE on 2018/07/11.
+//  Copyright © 2018 YOSUKE. All rights reserved.
+//
+
+import Foundation
---
+++ TaskApp/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  TaskApp
+//
+//  Created by YOSUKE on 2018/07/10.
+//  Copyright © 2018 YOSUKE. All rights reserved.
+//
+
+import Foundation
