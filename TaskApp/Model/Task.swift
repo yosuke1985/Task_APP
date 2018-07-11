0d6850a0683c92b645b4565a2b9746876acc8650
YOSUKE NAKAYAMA <ihatenonoildressing@gmail.com>
2018-07-11 09:02:24 +0900

    task, user model

--- TaskApp/Model/Task.swift
+++ TaskApp/Model/Task.swift
@@ -7,3 +7,5 @@
 //
 
 import Foundation
+
+// Refactoring Task
--- TaskApp/Model/User.swift
+++ TaskApp/Model/User.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import Firebase
+
+struct User {
+    let uid: String
+    let email: String
+    
+    init(authData: Firebase.User) {
+        uid = authData.uid
+        email = authData.email!
+    }
+    
+    init(uid: String, email: String) {
+        self.uid = uid
+        self.email = email
+    }
+}
